9481e7787f0ecd8750227f8e8819897ef36e5c88
Alistair Keiller <alistair@keiller.net>
2025-03-11 22:54:45 -0700

    start renaming (copy from drift config)

--- source/wheeledlab_tasks/wheeledlab_tasks/__init__.py
+++ source/wheeledlab_tasks/wheeledlab_tasks/__init__.py
@@ -7,40 +7,53 @@ import gymnasium as gym
 from .drifting import MushrDriftRLEnvCfg, MushrDriftPlayEnvCfg
 from .visual import MushrVisualRLEnvCfg, MushrVisualPlayEnvCfg
 from .elevation import MushrElevationRLEnvCfg, MushrElevationPlayEnvCfg
+from .racing import F1tenthRaceRLEnvCfg, F1tenthRacePlayEnvCfg
 import wheeledlab_tasks.drifting.config.agents.mushr as mushr_drift_agents
 import wheeledlab_tasks.visual.config.agents.mushr as mushr_visual_agents
 import wheeledlab_tasks.elevation.config.agents.mushr as mushr_elevation_agents
+import wheeledlab_tasks.racing.config.agents.f1tenth as f1tenth_race_agents
 
 gym.register(
     id="Isaac-MushrDriftRL-v0",
-    entry_point='isaaclab.envs:ManagerBasedRLEnv',
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point":MushrDriftRLEnvCfg,
+        "env_cfg_entry_point": MushrDriftRLEnvCfg,
         "rsl_rl_cfg_entry_point": f"{mushr_drift_agents.__name__}.rsl_rl_ppo_cfg:MushrPPORunnerCfg",
-        "play_env_cfg_entry_point": MushrDriftPlayEnvCfg
-    }
+        "play_env_cfg_entry_point": MushrDriftPlayEnvCfg,
+    },
 )
 
 
 gym.register(
     id="Isaac-MushrVisualRL-v0",
-    entry_point='isaaclab.envs:ManagerBasedRLEnv',
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point":MushrVisualRLEnvCfg,
+        "env_cfg_entry_point": MushrVisualRLEnvCfg,
         "rsl_rl_cfg_entry_point": f"{mushr_visual_agents.__name__}.rsl_rl_ppo_cfg:MushrPPORunnerCfg",
-        "play_env_cfg_entry_point": MushrVisualPlayEnvCfg
-    }
+        "play_env_cfg_entry_point": MushrVisualPlayEnvCfg,
+    },
 )
 
 gym.register(
     id="Isaac-MushrElevationRL-v0",
-    entry_point='isaaclab.envs:ManagerBasedRLEnv',
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": MushrElevationRLEnvCfg,
         "rsl_rl_cfg_entry_point": f"{mushr_elevation_agents.__name__}.rsl_rl_ppo_cfg:MushrPPORunnerCfg",
-        "play_env_cfg_entry_point": MushrElevationPlayEnvCfg
-    }
+        "play_env_cfg_entry_point": MushrElevationPlayEnvCfg,
+    },
+)
+
+gym.register(
+    id="Isaac-F1tenthRaceRL-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": F1tenthRaceRLEnvCfg,
+        "rsl_rl_cfg_entry_point": f"{f1tenth_race_agents.__name__}.rsl_rl_ppo_cfg:F1tenthPPORunnerCfg",
+        "play_env_cfg_entry_point": F1tenthRacePlayEnvCfg,
+    },
 )
---
+++ source/wheeledlab_tasks/wheeledlab_tasks/racing/__init__.py
@@ -0,0 +1,1 @@
+from .f1tenth_race_env_cfg import F1tenthRaceRLEnvCfg, F1tenthRacePlayEnvCfg
